e558b0caf058946e4f9e5bfab3ddafcd041a0f7f
McKenzie Parkhurst <mckenzie_parkhurst@sfu.ca>
2026-04-09 14:59:40 -0700

    In progress testing procedural generation.

---
+++ dungeons/scripts/procedural_generation.gd
@@ -0,0 +1,19 @@
+extends TileMapLayer
+
+var moisture = FastNoiseLite.new()
+var temperature = FastNoiseLite.new()
+var altitude = FastNoiseLite.new()
+
+//Chunk dimensions
+var width = 128
+var height = 128
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
